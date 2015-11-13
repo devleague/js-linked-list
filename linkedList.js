f93c83a72ba5a87005e57a6839e148c16e69065d
Jonathan AhNee <jonahnee@gmail.com>
2015-11-12 21:34:24 -1000

    work in progress

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,43 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var head = null;
+  var tail = null;
+  var length = 0;
+
+  var newLinkedList = {
+    
+    getHead: function() {
+      return head;
+    },
+
+    getTail: function() {
+      return tail;
+    },
+
+    add: function(x) {
+      var newNode = {
+        value: x,
+        next: null,
+      };
+
+      return newNode;
+    },
+
+    remove: function(x) {
+
+    },
+
+    get: function(x) {
+    },
+
+    insert: function(x, y) {
+
+    }
+  };
+    
+
+
+  return newLinkedList;
 
 }
