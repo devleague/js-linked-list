314d76e3d81d81e6b40f862d19ed1f42b4c94791
Tanachi <tanachige@gmail.com>
2016-05-21 12:09:20 -1000

    Added list object

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,13 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
-
+  var head = null;
+  var tail = null;
+  var node = function() {
+    this.value = null;
+    this.next = null;
+  };
+  var list = {
+  };
+  return list;
 }
