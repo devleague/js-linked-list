8cf165a9b658dbc729af9de3407aee297e46ca2e
sgalutira <sheena.galutira@gmail.com>
2016-05-21 10:35:00 -1000

    return module object 2nd test

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,11 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var linkedList = {};
+  function getHead(){
+  }
+    return linkedList;
+
+
 
 }
