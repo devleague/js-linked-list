fa8150697302a0979e40e75eb95dd5c69a803c44
sgalutira <sheena.galutira@gmail.com>
2016-05-21 10:43:58 -1000

    Module Object methods in linked list obj

--- linkedList.js
+++ linkedList.js
@@ -4,8 +4,31 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
-  var linkedList = {};
+  var linkedList = {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert
+
+
+  };
+
   function getHead(){
+
+  }
+  function getTail(){
+
+  }
+  function add(){
+  }
+
+  function remove(){
+  }
+  function get(){
+  }
+  function insert(){
   }
     return linkedList;
 
