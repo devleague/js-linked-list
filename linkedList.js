9a61c7e62fb2db4fc7e8a045c20b015b2294a2fe
Christie R <creindle@hawaii.edu>
2016-05-21 10:02:34 -1000

    I'm stuck...

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,28 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+var linkedListGenerator = function (){
 
-}
+  var newLinkedList = {};
+
+  newLinkedList.getHead = function (){
+      return newLinkedList[0].value;
+  };
+
+  newLinkedList.getTail = function (){
+  };
+
+  newLinkedList.add = function (){
+  };
+
+  newLinkedList.remove = function (){
+  };
+
+  newLinkedList.get = function (){
+  };
+
+  newLinkedList.insert = function (){
+  };
+
+return newLinkedList;
+};
