c98d3f96dff91227f82e5011cb980c94f7350cc2
Nikki K <808nikz@gmail.com>
2016-05-21 10:25:31 -1000

    return module object

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,21 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+		return{
+			value:"Ready Player One",
+			next:{
+				value:"1982",
+				next:{
+					value:"Neuromancer",
+					next:{
+						value:"Snow Crash",
+						next: null
+					}
+				}
+			}
+		};
 
+	function getHead(){
+
+	}
 }
