9a0382f7cb80b52c5f5968a492a942fe39fb6ce4
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-05 15:26:48 -1000

    gets head and tail

--- linkedList.js
+++ linkedList.js
@@ -4,7 +4,31 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  const head = null;
+  const tail = null;
+
+  function getHead(){
+    return head;
+  };
+
+  function getTail(){
+    return tail;
+  };
+
+  function add(){};
+
+  function remove(){};
+
+  function get(){};
+
+  function insert(){};
+
   return {
-    // getHead: newLinkedList.getHead(),
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert,
   }
 }
